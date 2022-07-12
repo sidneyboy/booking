adb4d3c68d315c9e56f899bf10f4779ee1370085
sidneyboy <100180350+sidneyboy@users.noreply.github.com>
2022-07-12 08:14:38 +0800

    Updated

---
+++ app/Models/Agent_user.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Agent_user extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'agent_id',
+        'agent_name',
+        'agent_image',
+    ];
+}
---
+++ database/migrations/2022_07_11_114940_create_agent_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('agent_users', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('agent_name',100);
+            $table->Integer('agent_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('agent_users');
+    }
+};
