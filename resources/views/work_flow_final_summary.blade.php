66e440971b813223a94cb56d3e08fc0e271e6105
sidneyboy <100180350+sidneyboy@users.noreply.github.com>
2022-07-13 15:40:38 +0800

    updated

--- resources/views/work_flow_final_summary.blade.php
+++ resources/views/work_flow_final_summary.blade.php
@@ -15,22 +15,42 @@
                     <td style="text-align: right">{{ $sales_order_final_quantity[$data->id] }}</td>
                     <td style="text-align: right">
                         @if ($customer_principal_price->price_level == 'price_1')
-                            {{ $data->price_1 }}
+                            @php
+                                $unit_price = $data->price_1;
+                            @endphp
+                            {{ number_format($data->price_1, 2, '.', ',') }}
                         @elseif($customer_principal_price->price_level == 'price_2')
-                            {{ $data->price_2 }}
+                            @php
+                                $unit_price = $data->price_2;
+                            @endphp
+                            {{ number_format($data->price_2, 2, '.', ',') }}
                         @elseif($customer_principal_price->price_level == 'price_3')
-                            {{ $data->price_3 }}
+                            @php
+                                $unit_price = $data->price_3;
+                            @endphp
+                            {{ number_format($data->price_3, 2, '.', ',') }}
                         @elseif($customer_principal_price->price_level == 'price_4')
-                            {{ $data->price_4 }}
+                            @php
+                                $unit_price = $data->price_4;
+                            @endphp
+                            {{ number_format($data->price_4, 2, '.', ',') }}
                         @endif
                     </td>
-                    <td>
+                    <td style="text-align: right">
                         @php
-                            $sub_total = $
+                            $sub_total = $unit_price * $sales_order_final_quantity[$data->id];
+                            echo number_format($sub_total, 2, '.', ',');
+                            $sum_total[] = $sub_total;
                         @endphp
                     </td>
                 </tr>
             @endforeach
         </tbody>
+        <tfoot>
+            <tr>
+                <th colspan="3">Total</th>
+                <th style="text-align: right">{{ number_format(array_sum($sum_total), 2, '.', ',') }}</th>
+            </tr>
+        </tfoot>
     </table>
 </div>
